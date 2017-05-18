c64a29b197d33c9a3e897feecfb0b165b7bf271d
Rakibul Sayed <sarahasa_@lib1202-09.lib.jyu.fi>
2017-05-18 17:23:36 +0300

    new components- form and result have been created, and styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Searchquery } from './searchquery.interface';
+//import { Searchquery } from './searchquery.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,25 +7,7 @@ import { Searchquery } from './searchquery.interface';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public paperName;
-  public dateFrom ;
-  public dateTo;
 
-  public searchquery : Searchquery;
-
-search(formInputs) {
-  var dates = formInputs.split(',');
-  this.dateFrom = dates[0];
-  this.dateTo= dates[1]; 
-
-  console.log(this.paperName);
-  console.log(this.dateFrom);
-  console.log(this.dateTo);
-}
-
-submitForm(){
-  //do nothing
-  //console.log();
-}
+  //public searchquery : Searchquery;
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
